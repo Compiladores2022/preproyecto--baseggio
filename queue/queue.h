08676b8dbca40ea983513c5956c51ae1500687a5
gabrielbaseggio <baseggioaxel@gmail.com>
2022-08-29 22:02:48 -0300

    Documentation

--- queue/queue.h
+++ queue/queue.h
@@ -10,10 +10,21 @@ typedef struct {
    int length; 
 } Queue;
 
+/* The 'length' function returns the length of the 'queue'. */
 int length(Queue queue);
+
+/* The 'isEmpty' function returns ZERO iff the 'queue' is not empty. Otherwise it returns a non-zero number. */
 int isEmpty(Queue queue);
+
+/* The 'head' function returns a pointer to void which corresponds to the 'data' stored at the head of the 'queue'. */
 void* head(Queue queue);
+
+/* The 'enqueue' function add the element 'data' to the 'queue'. To prevent data corruption the caller must pass as parameter a copy of 'data'. */
 void enqueue(Queue* queue, void* data);
+
+/* The 'dequeue' function remove the head of the 'queue'. */
 void dequeue(Queue* queue);
+
+/* The 'construct' function initializes the 'queue'. */
 void construct(Queue* queue);
 #endif
--- stack/stack.h
+++ stack/stack.h
@@ -10,11 +10,22 @@ typedef struct {
     int length;
 } Stack;
 
+/* The 'length' function returns the length of the 'stack'. */
 int length(Stack stack);
+
+/* The 'isEmpty' function returns ZERO iff the 'stack' is not empty. Otherwise it returns a non-zero number.  */
 int isEmpty(Stack stack);
+
+/* The 'peek' function returns a pointer to void which corresponds to the 'data' stored at the peek of the 'stack' */
 void* peek(Stack stack);
+
+/* The 'push' function add the element 'data' to the 'stack'. To prevent data corruption the caller must pass as parameter a copy of 'data'. */
 void push(Stack* stack, void* data);
+
+/* The 'pop' function remove the peek of the 'stack'. */
 void pop(Stack* stack);
+
+/* The 'construct' function initializes the 'stack'. */
 void construct(Stack* stack);
 
 #endif
