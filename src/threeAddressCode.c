f20699eeecd40e3fb0fd27bfd1f1aca0f426200f
gabrielbaseggio <baseggioaxel@gmail.com>
2022-09-14 17:58:29 -0300

    Leaves renamed

--- src/threeAddressCode.c
+++ src/threeAddressCode.c
@@ -21,8 +21,10 @@ void addInstruction(ThreeAddressCode* threeAddressCode, Instruction instruction)
 Symbol* generateIntermediateCode(ASTNode* node, ThreeAddressCode* threeAddressCode) {
     if(node) {
         if(isLeave(node)) {
+	    if(node->symbol->flag != flag_IDENTIFIER) {
 	    node->symbol->name = (char*) malloc(sizeof(char));
-            sprintf(node->symbol->name, "%d", node->symbol->value);	    
+            sprintf(node->symbol->name, "%d", node->symbol->value);
+            }	    
             return node->symbol; 
         }
         
