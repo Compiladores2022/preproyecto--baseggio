5f26040441955c12e86ba881ef5345090256b272
gabrielbaseggio <baseggioaxel@gmail.com>
2022-10-16 17:57:36 -0400

    translateMULTIPLICATION implementation

--- src/assemblerGenerator.c
+++ src/assemblerGenerator.c
@@ -27,7 +27,9 @@ void translateSUBSTRACTION(FILE* fp, Instruction instruction) {
 }
 
 void translateMULTIPLICATION(FILE* fp, Instruction instruction) {
-
+	fprintf(fp, "\n\tmovq %s, %%r10", translateOperand(*instruction.fstOperand));
+	fprintf(fp, "\n\timul %s, %%r10", translateOperand(*instruction.sndOperand));
+	fprintf(fp, "\n\tmovq %%r10, -%d(%%rbp)", getOffset(*instruction.dest));
 }
 
 void translateDIVISION(FILE* fp, Instruction instruction) {
