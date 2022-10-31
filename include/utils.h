8f527c53299cbf4329c3d10b877579e69ecc0e99
gabrielbaseggio <baseggioaxel@gmail.com>
2022-10-31 11:53:49 -0300

    fixed typing error in define directive

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,5 @@
 #ifndef UTILS_H
-#define UTILS_J
+#define UTILS_H
 
 /* The 'allocateChar' function allocate a linear region of bytes for char that is size bytes long */
 char* allocateChar(int size);
