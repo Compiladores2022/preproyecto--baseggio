fcefb7fcd51c45357351a2c575b2a916870b38d1
gabrielbaseggio <baseggioaxel@gmail.com>
2022-10-31 10:02:41 -0300

    corrected indentation

--- src/flag.c
+++ src/flag.c
@@ -2,73 +2,77 @@
 
 char* flagToString(Flag flag) {
 	switch (flag) {
-	    case flag_IDENTIFIER:
-		    return "ID";
-	    case flag_ADDITION:
-		    return "+";
-	    case flag_SUBSTRACTION:
-		    return "-";
-	    case flag_MULTIPLICATION:
-		    return "*";
-	    case flag_DIVISION:
-		    return "/";
-	    case flag_MOD:
-		    return "%";
-            case flag_LT:
-		    return "<";
-	    case flag_GT:
-		    return ">";
-	    case flag_EQT:
-		    return "==";
-	    case flag_OR:
-		    return "||";
-	    case flag_AND:
-		    return "&&";
-	    case flag_MINUS:
-		    return "-";
-	    case flag_NEG:
-		    return "!";
-	    case flag_ASSIGNMENT:
-		    return "=";
-	    case flag_SEMICOLON:
-		    return ";";
-	    case flag_VALUE_INT:
-		    return "vInt";
-	    case flag_VALUE_BOOL:
-		    return "vBool";
-	    case flag_RETURN:
-		    return "return";
-            case flag_IF:
-                    return "if";
-            case flag_IF_ELSE:
-                    return "if-then-else";
-            case flag_WHILE:
-                    return "while";
-            case flag_METHOD_DECLARATION:
-		    return "method declaration";
-	    case flag_METHOD_CALL:
-		    return "method call";
-	    case flag_PARAM:
-		    return "param";
-	    case flag_GLOBAL_VAR_DECL:
-	    	    return "global var declaration";
-	    default:
-		    return "NRF";
+		case flag_IDENTIFIER:
+			return "ID";
+		case flag_ADDITION:
+			return "+";
+		case flag_SUBSTRACTION:
+			return "-";
+		case flag_MULTIPLICATION:
+			return "*";
+		case flag_DIVISION:
+			return "/";
+		case flag_MOD:
+			return "%";
+		case flag_LT:
+			return "<";
+		case flag_GT:
+			return ">";
+		case flag_EQT:
+			return "==";
+		case flag_OR:
+			return "||";
+		case flag_AND:
+			return "&&";
+		case flag_MINUS:
+			return "-";
+		case flag_NEG:
+			return "!";
+		case flag_ASSIGNMENT:
+			return "=";
+		case flag_SEMICOLON:
+			return ";";
+		case flag_VALUE_INT:
+			return "vInt";
+		case flag_VALUE_BOOL:
+			return "vBool";
+		case flag_RETURN:
+			return "return";
+		case flag_IF:
+			return "if";
+		case flag_IF_ELSE:
+			return "if-then-else";
+		case flag_WHILE:
+			return "while";
+		case flag_METHOD_DECLARATION:
+			return "method declaration";
+		case flag_METHOD_CALL:
+			return "method call";
+		case flag_PARAM:
+			return "param";
+		case flag_GLOBAL_VAR_DECL:
+			return "global var declaration";
+		default:
+			return "NRF";
 	}
 }
 
 int isAnArithmeticBinaryOperator(Flag flag) {
-    return flag == flag_ADDITION || flag == flag_SUBSTRACTION || flag == flag_MULTIPLICATION || flag == flag_DIVISION || flag == flag_MOD;
+	return flag == flag_ADDITION 
+		|| flag == flag_SUBSTRACTION 
+		|| flag == flag_MULTIPLICATION 
+		|| flag == flag_DIVISION 
+		|| flag == flag_MOD;
 }
 
 int isABooleanBinaryOperator(Flag flag) {
-    return flag == flag_OR || flag == flag_AND;
+	return flag == flag_OR || flag == flag_AND;
 }
 
 int isABinaryOperator(Flag flag) {
-    return isAnArithmeticBinaryOperator(flag) || isABooleanBinaryOperator(flag);
+	return isAnArithmeticBinaryOperator(flag) || isABooleanBinaryOperator(flag);
 }
 
 int isAConstant(Flag flag) {
-    return flag == flag_VALUE_INT || flag == flag_VALUE_BOOL;
+	return flag == flag_VALUE_INT || flag == flag_VALUE_BOOL;
 }
