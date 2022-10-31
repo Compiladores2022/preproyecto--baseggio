72d011a7b84f45715a23377780e9c0922ab3e61a
gabrielbaseggio <baseggioaxel@gmail.com>
2022-10-31 10:05:30 -0300

    corrected indentation

--- src/type.c
+++ src/type.c
@@ -1,14 +1,14 @@
 #include "type.h"
 
 char* typeToString(Type type) {
-    switch (type) {
-        case TYPE_INT:
-            return "Int";
-        case TYPE_BOOL:
-            return "Bool";
-	case TYPE_VOID:
-	    return "void";
-        default:
-            return "NRF";
-    }
+	switch (type) {
+		case TYPE_INT:
+			return "Int";
+		case TYPE_BOOL:
+			return "Bool";
+		case TYPE_VOID:
+			return "void";
+		default:
+			return "NRF";
+	}
 }
