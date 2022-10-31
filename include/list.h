cb245b498dd783f6bd383c5e7dd9f2e9730cfa5c
gabrielbaseggio <baseggioaxel@gmail.com>
2022-10-31 11:21:02 -0300

    modified documentation

--- include/list.h
+++ include/list.h
@@ -15,52 +15,52 @@ typedef struct {
 } List;
 
 /*
- * Returns the number of elements in this list.
+ * The 'length' function returns the number of elements in this list.
  */
 int length(List list);
 
 /*
- * Returns true if this list contains no elements.
+ * The 'isEmpty' function returns true if this list contains no elements.
  */
 int isEmpty(List list);
 
 /*
- * Returns the first element of this list.
+ * The 'head' function returns the first element of this list.
  */
 void* head(List list);
 
 /*
- * Returns the element at the specified position in this list.
+ * The 'getByIndex' function returns the element at the specified position in this list.
  */
 void* getByIndex(List list, int index);
 
 /*
- * Returns the first ocurrence of the specified element from this list.
+ * The 'lookUp' function returns the first ocurrence of the specified element from this list.
  */
 void* lookUp(List list, int (*f) (void*, void*), void* e);
 
 /*
- * Appends the specified element to the end of this list.
+ * The 'add' function appends the specified element to the end of this list.
  */
 void add(List* list, void* data, size_t dataSize, int isPtr);
 
 /*
- * Inserts the specified element at the specified position in this list.
+ * The 'addByIndex' inserts the specified element at the specified position in this list.
  */
 void addByIndex(List* list, void* data, size_t dataSize, int index, int isPtr);
 
 /*
- * Removes the first element of this list
+ * The 'removeFirst' function removes the first element of this list
  */
 void removeFirst(List* list);
 
 /*
- * Constructs an empty list
+ * The 'constructList' function constructs an empty list<
  */
 void constructList(List* list);
 
 /*
- * Prints the elements of this list
+ * The 'print' function prints the elements of this list to the STDOUT
  */
 void print(List list, void (*f) (void*));
 #endif
